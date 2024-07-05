5d45971cdb55d279ff35f84776d3da8956633122
Tanuja Mohanty <150986880+Tanuja9090@users.noreply.github.com>
2024-07-06 01:00:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const SignInBtnLink = document.querySelector('.SignInBtn-link');
+const SignUpBtnLink = document.querySelector('.SignUpBtn-link');
+const wrapper = document.querySelector('.wrapper');
+
+SignUpBtnLink.addEventListener('click', () => {wrapper.classList.toggle('active');
+
+});
